0eadf6b72103f30f2a5f6af2194eec2f25f1b68b
Jenny <19268979+jz333@users.noreply.github.com>
2022-01-29 21:09:26 -0500

    Add files via upload

---
+++ wsClient.rb
@@ -0,0 +1,35 @@
+require 'faye/websocket'
+require 'eventmachine'
+
+# EM#run will start the EM reactor and keep running until told to stop
+# EM#stop or EM#stop_event_loop
+EM.run do
+  ws = Faye::WebSocket::Client.new("ws://localhost:4567/")
+
+  ws.on :open do |event|
+    puts "Connected to the chat room server!"
+  end
+
+  ws.on :message do |event|
+    puts event.data
+
+
+    # use threads to prevent blocking on :message handler
+    # if thread is not used, messages to other clients will
+    # be delayed
+    Thread.new {
+      msg = $stdin.gets.chomp
+      ws.send(msg)
+    }
+  end
+
+  ws.on :close do |event|
+    #puts [:close, event.code, event.reason]
+    ws = nil
+    puts "Cannot connect server. Exiting..."
+    EM.stop   # if server terminates, stop EM and exit
+  end
+
+
+
+end
---
+++ wsServer.rb
@@ -0,0 +1,103 @@
+# ##############################################
+# # Using faye-websocket
+# ##############################################
+
+require 'faye/websocket'
+require 'rack'
+
+class MyServer
+
+  def initialize(socket_port, socket_host)
+    @socket_port = socket_port
+    @socket_host = socket_host
+
+    @clients = []
+    @wsServer = wsServer
+    start_server
+  end
+
+  def wsServer
+    wsServer = lambda do |env|
+    # inspect env to see if the incoming request is a WebSocket request
+      if Faye::WebSocket.websocket?(env)
+        # It is a WebSocket connection
+        ws = Faye::WebSocket.new(env)
+
+
+        # open gets invoked when a new connection to the server happens
+        # store a newly connected client in the @clients array
+        ws.on :open do |event|
+          puts "Connection open with ws #{ws.object_id}."
+          ws.send("Welcome to the chat room! Start chatting!")
+          @clients << ws
+
+          puts "In the chat room: "
+          @clients.each {|client| puts client}
+        end
+
+
+        # message gets invoked when a WebSocket message is received
+        # by the server. The event object passed in has a 'data' attribute
+        # which is the message being sent
+        ws.on :message do |event|
+          puts "Client: #{event.data}"
+
+          # broadcase to clients
+          @clients.each { |client|
+            if client != ws
+                client.send("#{ws}: #{event.data}")
+            end
+          }
+
+        end
+
+
+        # close gets invoked when the client closes the connection
+        # remove the client from client list when disconnected
+        ws.on :close do |event|
+          puts "Closing the connection with #{ws}..."
+          puts "close, #{ws.object_id}, #{event.code}, #{event.reason}"
+
+          @clients.delete(ws)
+          @clients.each { |client| client.send("#{ws} has left.")}
+          ws = nil
+        end
+
+
+        # Return async Rack response ?????
+        ws.rack_response
+      else
+        # Normal HTTP request
+        [200, { 'Content-Type' => 'text/plain' }, ['Hello, this is a normal HTTP Request.']]
+      end
+    end
+
+  end
+
+
+  def start_server
+    # run the server on thin
+    # load adapter to be able to upgrade handshake to websocket protocol
+    Faye::WebSocket.load_adapter('thin')
+
+    # use thin.run(wsServer,...) to run the lambda and get env
+    thin = Rack::Handler.get('thin')
+    thin.run(@wsServer, :Host => @socket_host, :Port => @socket_port) do |server|
+      #You can set options on the server here, for example to set up SSL:
+      # if secure
+      #   server.ssl_options = {
+      #     :private_key_file => 'path/to/ssl.key',
+      #     :cert_chain_file  => 'path/to/ssl.crt'
+      #   }
+      #   server.ssl = true
+    end
+
+  end
+
+end
+
+
+
+MyServer.new(4567, 'localhost')
+
+
